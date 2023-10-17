1bf37528fc749383f2fdaa9343590890220baedd
douglas-farias <140763165+douglas-farias@users.noreply.github.com>
2023-10-16 22:50:10 -0300

    Update catalogo.js

--- catalogo.js
+++ catalogo.js
@@ -1,42 +1,11 @@
-// window.onload = function() {
-//     var filmesDiv = document.getElementById('filmes');
+let buscarFilme = document.getElementById("buscarFilme");
 
-    
+buscarFilme.onclick = () => {
 
-//         var filmes = [
-//             { title: 'Besouro Azul', img: src="img/besouro azul.svg" },
-//             { title: 'Agente Stone', img: src="img/agente stone.svg" },
-//             { title: 'Oppenheimer', img: src="img/Oppenheimer.svg" },
-//             { title: 'Barbie', img: src="img/barbie.svg" },
-//             { title: 'Megatubarão 2', img: src="img/megatubarao_2.svg" },
-//             { title: 'Elementos', img: src="img/elementos.svg" },
-//             { title: 'Johnny Depp x Amber Heard', img: src="img/Johnny Depp x Amber Heard.svg" },
-//             { title: 'Homem-Aranha: Através do Aranhaverso', img: src="img/Homem-Aranha_ Através do Aranhaverso.svg" },
-//             { title: 'Guardiões da Galáxia Vol.3', img: src="img/Guardiões da Galáxia_ Vol_3.svg" },
-//             { title: 'Velozes & Furiosos 10', img: src="img/Velozes & Furiosos 10.svg" }
-//         ];
-//         for (var i = 0; i < filmes.length; i++) {
-//             var filmeDiv = document.createElement('div');
-//             filmeDiv.className = 'filme col-md-4';
+    let filmeDigitado = document.getElementById("filmeDigitado");
 
-//             var img = document.createElement('img');
-//             img.src = movie.image;
-//             item.appendChild(img);
-
-//         var title = document.createElement('h2');
-//         title.textContent = filmes[i].title;
-//         filmeDiv.appendChild(title);
-
-        
-
-//         filmesDiv.appendChild(filmeDiv);
-//     }
-// }
-
-window.onload = function() {
-
-    let url = 'https://www.omdbapi.com/?i=tt3896198&apikey=a0537c9b';
-    let urlPoster = 'https://img.omdbapi.com/?i=tt3896198&apikey=a0537c9b';
+    let url = 'https://www.omdbapi.com/?apikey=a0537c9b&t=' + filmeDigitado.value;
+    console.log(url)
 
     fetch(url)
         .then(response => response.json())
@@ -45,17 +14,22 @@ window.onload = function() {
 
 function exibirFilme(filme) {
 
-let title = document.getElementById("title");
-let year = document.getElementById("year");
-let rated = document.getElementById("year");
+let titulo = document.getElementById("titulo");
+let ano = document.getElementById("ano");
+let duracao = document.getElementById("duracao");
+let classificacaoIndicativa = document.getElementById("classificacaoIndicativa");
 
-let divImg = document.getElementById("poster")
 
-divImg.innerText = ""
+titulo.innerText = filme.Title
+ano.innerText = filme.Year
+duracao.innerText = filme.Runtime
+classificacaoIndicativa.innerText = filme.Rated
+
+let divImg = document.getElementById("poster");
+divImg.innerText = "";
 
 let poster = document.createElement("img")
-poster.src = filme[0].Poster
+poster.src = filme.Poster
 
 divImg.append(poster)
-
 }
